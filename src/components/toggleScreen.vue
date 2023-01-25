018e1561c1dc92b9f89ed23a65301445d00c877d
EKar <Earthb0y@outlook.com>
2023-01-26 00:04:09 +0800

    feat:toggleScreen

---
+++ src/components/toggleScreen.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+import { NButton } from 'naive-ui'
+const props = defineProps({
+  isFullScreen: Boolean,
+})
+const screenState = computed(() => {
+  return props.isFullScreen ? '窗口' : '全屏'
+})
+</script>
+
+<template>
+  <!-- <i class="i-carbon-fit-to-screen mr-2 text-lg opacity-80 hover:opacity-100 transition-all cursor-pointer duration-100" /> -->
+  <NButton secondary>
+    {{ screenState }}
+  </NButton>
+</template>
