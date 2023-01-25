ff5b12a06a8f69e4a45a8636b6ebaf3de2265136
EKar <Earthb0y@outlook.com>
2023-01-25 22:50:12 +0800

    feat: github link

---
+++ src/components/Github.vue
@@ -0,0 +1,8 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <a href="https://github.com/Ekarmore/Naive-Admin" target="_blank" />
+  <i class="i-carbon-logo-github mr-2 text-lg opacity-80 hover:opacity-100 transition-all ease-in-out cursor-pointer" />
+</template>
