4808b3821b3a0843f20c8f2ecf157c0f23f69e02
EKar <Earthb0y@outlook.com>
2023-01-26 20:21:51 +0800

    feat:breadCrumb

---
+++ src/components/breadCrumb.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { NBreadcrumb, NBreadcrumbItem } from 'naive-ui'
+</script>
+
+<template>
+  <NBreadcrumb>
+    <NBreadcrumbItem>
+      <n-icon /> 北京总行
+    </NBreadcrumbItem>
+    <NBreadcrumbItem>
+      <n-icon /> 天津分行
+    </NBreadcrumbItem>
+    <NBreadcrumbItem>
+      <n-icon /> 平山道支行
+    </NBreadcrumbItem>
+  </NBreadcrumb>
+</template>
