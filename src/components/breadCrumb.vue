3db279a6b870a6c13c067179f52de66a5c3d1715
EKar <Earthb0y@outlook.com>
2023-02-02 23:17:58 +0800

    feat:breadCrumb

--- src/components/breadCrumb.vue
+++ src/components/breadCrumb.vue
@@ -1,19 +1,23 @@
 <script setup lang="ts">
 import { NBreadcrumb, NBreadcrumbItem } from 'naive-ui'
 const route = useRoute()
+const router = useRouter()
 
-const pathList = computed(() => {
-  return route.path.slice(1).split('/')
-})
 const List = computed(() => {
-  return route.matched
+  return route.matched.filter(item => !!item.meta?.title)
 })
+
+const handleBreadClick = (path: string) => {
+  if (path === route.path)
+    return
+  router.push(path)
+}
 </script>
 
 <template>
   <NBreadcrumb>
-    <NBreadcrumbItem v-for="item in pathList" :key="item">
-      {{ item }}
+    <NBreadcrumbItem v-for="item in List" :key="item.path" @click="handleBreadClick(item.path)">
+      {{ item.meta.title }}
     </NBreadcrumbItem>
   </NBreadcrumb>
 </template>
