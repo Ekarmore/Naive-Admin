4a2142886217e1ca1f10fc69e4626b6b995fd2df
EKar <Earthb0y@outlook.com>
2023-02-02 23:45:06 +0800

    feat:404page

--- src/pages/[...all].vue
+++ src/pages/[...all].vue
@@ -1,14 +1,23 @@
 <script setup lang="ts">
+import { NButton } from 'naive-ui'
 const { t } = useI18n()
+const router = useRouter()
 </script>
 
 <template>
-  <div>
-    {{ t('not-found') }}
+  <div h="screen" text="4xl" font="bold" class="flex flex-col items-center mt-20">
+    <div class="h-12 flex items-center">
+      {{ t('not-found') }}
+    </div>
+    <div class="mt-2">
+      <NButton @click="router.back()">
+        返回
+      </NButton>
+    </div>
   </div>
 </template>
 
 <route lang="yaml">
 meta:
-  layout: 404
+  title: 404
 </route>
