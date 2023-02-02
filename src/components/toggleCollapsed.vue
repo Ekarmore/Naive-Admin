ddeb83690657c09afb161683f0f56c0a8ba1eb46
EKar <Earthb0y@outlook.com>
2023-02-02 23:22:28 +0800

    feat:toggleCollapsed

---
+++ src/components/toggleCollapsed.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { NButton } from 'naive-ui'
+import { storeToRefs } from 'pinia'
+const store = useAppStore()
+const { switchCollapsed } = storeToRefs(store)
+</script>
+
+<template>
+  <div class="flex  items-center">
+    <div class="p-2">
+      <NButton v-show="switchCollapsed" quaternary class="p-2" @click="store.toggleCollapsed">
+        <i class="i-grommet-icons-next" />
+      </NButton>
+      <NButton v-show="!switchCollapsed" quaternary class="p-2" @click="store.toggleCollapsed">
+        <i class="i-grommet-icons-previous" />
+      </NButton>
+    </div>
+    <BreadCrumb class="col-start-1" />
+  </div>
+</template>
