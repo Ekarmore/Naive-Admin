6e76519aef68058c0fee1c64e9a4d82e3d9ccf06
EKar <Earthb0y@outlook.com>
2023-02-05 23:03:31 +0800

    feat:toggleDark

---
+++ src/components/Header/toggleDarkTheme.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+import { NButton } from 'naive-ui'
+import { storeToRefs } from 'pinia'
+const store = useAppStore()
+const {
+  themeInfo,
+} = storeToRefs(store)
+</script>
+
+<template>
+  <NButton quaternary class="w-14" @click="store.toggleTheme">
+    {{ themeInfo }}
+  </NButton>
+</template>
