5f082a126f82d6ca1df832d186c738ae37bec143
EKar <Earthb0y@outlook.com>
2023-02-07 21:37:44 +0800

    feat:remove

--- src/components/Github.vue
+++
@@ -1,9 +0,0 @@
-<script lang="ts" setup>
-import { NButton } from 'naive-ui'
-</script>
-
-<template>
-  <a href="https://github.com/Ekarmore/Naive-Admin" target="_blank">
-    <NButton quaternary>Github</NButton>
-  </a>
-</template>
