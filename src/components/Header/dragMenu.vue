343a38305235e751d8f2f2fef09000333985e1f7
EKar <Earthb0y@outlook.com>
2023-02-07 23:52:00 +0800

    feat:dragMenu

---
+++ src/components/Header/dragMenu.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+import { NButton, NPopover } from 'naive-ui'
+</script>
+
+<template>
+  <div>
+    <NPopover trigger="hover" :show-arrow="false">
+      <template #trigger>
+        <NButton quaternary>
+          菜单
+        </NButton>
+      </template>
+      <toggleDarkTheme />
+      <Github />
+      <ToggleScreen />
+    </NPopover>
+  </div>
+</template>
