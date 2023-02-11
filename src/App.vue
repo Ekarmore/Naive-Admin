5c26875fcfa74b98ef245357a004c5ccae2f1d7f
EKar <Earthb0y@outlook.com>
2023-02-11 23:07:52 +0800

    feat:messageProvider

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { NMessageProvider } from 'naive-ui'
 // https://github.com/vueuse/head
 // you can use this to manipulate the document head in any components,
 // they will be rendered correctly in the html results with vite-ssg
@@ -22,7 +23,9 @@ useHead({
 </script>
 
 <template>
-  <RouterView />
+  <NMessageProvider>
+    <RouterView />
+  </NMessageProvider>
 </template>
 
 <style>
